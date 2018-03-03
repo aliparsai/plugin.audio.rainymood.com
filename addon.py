c7b8975e1e18fe81e84ee1446046d8bd2e61b2d2
Ali Parsai <ali.parsai@email.com>
2018-03-04 00:13:59 +0100

    Added two features:
    * dim screen after timeout
    * use 8 rainymood source audios

--- addon.py
+++ addon.py
@@ -1,25 +1,37 @@
-import os,time
+import os,threading
 import xbmc,xbmcgui,xbmcplugin,xbmcaddon
 
 addon = xbmcaddon.Addon()
+dimSetting = int(addon.getSetting('dim_setting'))
 img = xbmc.translatePath( os.path.join( addon.getAddonInfo('path'), 'rain.jpg' ) )
-url = "http://rainymood.com/audio1110/0.ogg"
+img_dim = xbmc.translatePath( os.path.join( addon.getAddonInfo('path'), 'rain-dim.jpg' ) )
+url = ["http://rainymood.com/audio1110/" + str(i) + ".ogg" for i in range(1, 8)]
 # short ogg file to test loop:
 # url = "http://images.wikia.com/starwars/images/f/f5/A_little_short.ogg"
 
 class MyPlayer(xbmc.Player):
   def __init__(self):
+    self.noiseIndex = 0
     self.playNoise()
 
   def onPlayBackEnded(self):
+    self.noiseIndex = 0 if self.noiseIndex >= 7 else self.noiseIndex + 1
     self.playNoise()
 
   def playNoise(self):
-    self.play(url)
+    self.play(url[self.noiseIndex])
 
 class MyWindow(xbmcgui.WindowDialog):
   def __init__(self):
-    self.addControl(xbmcgui.ControlImage(1, 1, 1280, 720, img))
+    self.dimTimer = threading.Timer(dimSetting, self.dimScreen)
+    self.imgNormal = xbmcgui.ControlImage(1, 1, 1280, 720, img)
+    self.imgDim = xbmcgui.ControlImage(1, 1, 1280, 720, img_dim)
+    self.addControl(self.imgNormal)
+    self.dimTimer.start()
+
+  def dimScreen(self):
+    self.removeControl(self.imgNormal)
+    self.addControl(self.imgDim)
 
   def onAction(self, action):
     xbmc.log(str(action.getId()))
