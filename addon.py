250d128be6453c037681878546606c03fe65793f
Ali Parsai <ali.parsai@email.com>
2018-03-10 10:10:20 +0100

    - Added sleep feature - Dim screen is disabled by default

--- addon.py
+++ addon.py
@@ -2,7 +2,12 @@ import os,threading
 import xbmc,xbmcgui,xbmcplugin,xbmcaddon
 
 addon = xbmcaddon.Addon()
-dimSetting = 60 * int('5' if addon.getSetting('dim_setting') == '' else addon.getSetting('dim_setting'))
+dimEnabled = True if addon.getSetting('dim_enabled') == 'true' else False
+dimTime = 60 * int('5' if addon.getSetting('dim_time') == '' else addon.getSetting('dim_time'))
+sleepEnabled = True if addon.getSetting('sleep_enabled') == 'true' else False
+sleepTime = 60 * int('30' if addon.getSetting('sleep_time') == '' else addon.getSetting('sleep_time'))
+
+
 img = xbmc.translatePath( os.path.join( addon.getAddonInfo('path'), 'rain.jpg' ) )
 img_dim = xbmc.translatePath( os.path.join( addon.getAddonInfo('path'), 'rain-dim.jpg' ) )
 url = ["http://rainymood.com/audio1110/" + str(i) + ".ogg" for i in range(1, 8)]
@@ -23,23 +28,30 @@ class MyPlayer(xbmc.Player):
 
 class MyWindow(xbmcgui.WindowDialog):
   def __init__(self):
-    self.dimTimer = threading.Timer(dimSetting, self.dimScreen)
     self.imgNormal = xbmcgui.ControlImage(1, 1, 1280, 720, img)
     self.imgDim = xbmcgui.ControlImage(1, 1, 1280, 720, img_dim)
     self.addControl(self.imgNormal)
-    self.dimTimer.start()
+    if dimEnabled:
+      self.dimTimer = threading.Timer(dimTime, self.dimScreen)
+      self.dimTimer.start()
+    if sleepEnabled:
+      self.sleepTimer = threading.Timer(sleepTime, self.closeAddon)
+      self.sleepTimer.start()
 
   def dimScreen(self):
     self.removeControl(self.imgNormal)
     self.addControl(self.imgDim)
+
+  def closeAddon(self):
+    xbmc.executebuiltin("PlayerControl(Stop)")
+    self.close()
 
   def onAction(self, action):
     xbmc.log(str(action.getId()))
     # 10 = ACTION_PREVIOUS_MENU, 13 = ACTION_STOP, 93 = ACTION_NAV_BACK
     # see https://github.com/xbmc/xbmc/blob/master/xbmc/input/Key.h
     if action==10 or action==13 or action==92:
-      xbmc.executebuiltin("PlayerControl(Stop)")
-      self.close()
+      self.CloseAddon()
 
 p = MyPlayer()
 w = MyWindow()
